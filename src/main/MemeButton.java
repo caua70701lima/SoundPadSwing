38b8fa632068d79fd0bcfe1845e1696783e33034
Cauã Lima <cauai8099@gmail.com>
2026-03-09 00:32:40 -0300

    refatoração da função de adicionar botão (adicionarBotao) para a classe especifica (MemeButton).

--- src/main/MemeButton.java
+++ src/main/MemeButton.java
@@ -1,4 +1,14 @@
 package main;
 
-public class MemeButton {
+public class MemeButton extends JButton {
+    public MemeButton(String img, String snd, String nam) {
+        super(nam);
+        var path = getClass().getResource("/resources/images" + img);
+        if(path != null) {
+            ImageIcon icon = new ImageIcon(path);
+            setIcon(icon);
+        } else {
+            System.out.println("Arquivo não encontrado!");
+        }
+    }
 }
