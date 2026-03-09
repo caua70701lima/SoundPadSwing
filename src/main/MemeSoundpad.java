b0a6b90016cbc276b8593722326459cfef3b5c1d
Cauã Lima <cauai8099@gmail.com>
2026-03-08 22:43:07 -0300

    Adicionada a função de adicionar botão com três botões adicionados de exemplo.

--- src/main/MemeSoundpad.java
+++ src/main/MemeSoundpad.java
@@ -1,4 +1,35 @@
 package main;
+import javax.swing.*;
+import java.awt.*;
 
-public class MemeSoundpad {
+public class MemeSoundpad extends JFrame {
+    JPanel painel;
+    public MemeSoundpad() {
+        setTitle("Soundpad com memes brasileiros.");
+        setSize(600, 600);
+
+        painel = new JPanel(new GridLayout(3,3,10,10));
+
+        adicionaBotao("calma.jpg","","Que isso?");
+        adicionaBotao("falcon.jpg","","8 Mil?");
+        adicionaBotao("makeTheL.jpg", "", "Faz o L");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(painel);
+        setVisible(true);
+    }
+    private void adicionaBotao(String img, String snd, String nam){
+
+        JButton botao = new JButton(nam);
+        ImageIcon icon = new ImageIcon(getClass().getResource("/resources/images/" + img));
+        botao.setIcon(icon);
+
+        painel.add(botao);
+        // Resolver a parte do player de som ainda
+    }
+
+    static void main(String[] args) {
+        new MemeSoundpad();
+
+    }
 }
