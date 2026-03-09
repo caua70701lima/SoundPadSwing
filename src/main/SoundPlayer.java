1df0e5271eb97322175b2fb38c1705ca5f882cb0
NarioMendonca <nario.sousa2014@gmail.com>
2026-03-09 20:50:38 -0300

    feat: criado classe SoundPlayer

--- src/main/SoundPlayer.java
+++ src/main/SoundPlayer.java
@@ -1,6 +1,30 @@
 package main;
+
+import java.io.File;
+import java.io.IOException;
+
 import javax.sound.sampled.*;
 
 public class SoundPlayer {
-
+    static public void tocarSom(String nomeArquivo) {
+        try {
+            File file = new File("src/resources/sounds/" + nomeArquivo);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException error) {
+            error.printStackTrace();
+            if (error instanceof UnsupportedAudioFileException) {
+                System.out.println("Tipo de arquivo de áudio não suportado");
+                return;
+            } else if (error instanceof IOException) {
+                System.out.println("arquivo de aúdio não encontrado");
+                return;
+            } else if (error instanceof LineUnavailableException) {
+                System.out.println("Linha de áudio não encontrado ou indisponível");
+                return;
+            }
+        }
+    }
 }
